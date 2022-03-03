e56a00d240632a798fd5f7c343dfdb64469e2b72
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-03-03 19:09:23 +0000

    style and references from form

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 // get references to interactive elements
 const theForm = document.getElementById("myForm");
+const drinkChoice = document.getElementsByName("drink");
+const sizeChoice = document.getElementById("size");
+
+
+
+// read input
+
+// output values
+
+// perform functions
+
+// listen to events
