0813c0a73355412cf6c2c6fc3ba2097c260f8b3d
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-03-07 19:17:09 +0000

    output area js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,25 @@
 const theForm = document.getElementById("myForm");
 const txtDrinkChoice = document.getElementsByName("drink");
 const txtSizeChoice = document.getElementById("size");
+const txtBaseSmoothie = document.getElementById("base-smoothie");
+const txtBaseMilkshake = document.getElementById("base-milkshake");
+const txtExtraOption = document.getElementById("extra");
 
 //buttons
 const btnAddOrder = document.getElementById("add-order");
 const btnSaveFav = document.getElementById("save-favourite");
 const btnOrderFav = document.getElementById("order-favourite");
 const btnPlaceOrder = document.getElementById("place-order");
+
+//output area
+const outputDrinkDetails = document.getElementById("drink-details"); //Small smoothie: banana, strawberries with apple juice.
+const outputCurrentPrice = document.getElementById("current-drink-price");
+
+const outputOrderDetails = document.getElementById("order-details"); // Display order details
+const outputItemPrice = document.getElementById("item-price"); // Display item price
+
+const outputSubtotalPrice = document.getElementById("subtotal-price"); // Total Price value
+const outputFinalMessage = document.getElementById("final-message"); // Final message
 
 // read input
 
