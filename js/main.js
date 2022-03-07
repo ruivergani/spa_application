02924a09ee5c27ed139163bf27a487607c0eb515
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-03-07 20:00:28 +0000

    function Place Order Done

--- js/main.js
+++ js/main.js
@@ -1,31 +1,44 @@
 // get references to interactive elements
-const theForm = document.getElementById("myForm");
-const txtDrinkChoice = document.getElementsByName("drink");
-const txtSizeChoice = document.getElementById("size");
-const txtBaseSmoothie = document.getElementById("base-smoothie");
-const txtBaseMilkshake = document.getElementById("base-milkshake");
-const txtExtraOption = document.getElementById("extra");
+const theForm = document.getElementById('myForm');
+const txtDrinkChoice = document.getElementsByName('drink');
+const txtSizeChoice = document.getElementById('size');
+const txtBaseSmoothie = document.getElementById('base-smoothie');
+const txtBaseMilkshake = document.getElementById('base-milkshake');
+const txtExtraOption = document.getElementById('extra');
 
 //buttons
-const btnAddOrder = document.getElementById("add-order");
-const btnSaveFav = document.getElementById("save-favourite");
-const btnOrderFav = document.getElementById("order-favourite");
-const btnPlaceOrder = document.getElementById("place-order");
+const btnAddOrder = document.getElementById('add-order');
+const btnSaveFav = document.getElementById('save-favourite');
+const btnOrderFav = document.getElementById('order-favourite');
+const btnPlaceOrder = document.getElementById('place-order');
 
 //output area
-const outputDrinkDetails = document.getElementById("drink-details"); //Small smoothie: banana, strawberries with apple juice.
-const outputCurrentPrice = document.getElementById("current-drink-price");
+const outputDrinkDetails = document.getElementById('drink-details'); //Small smoothie: banana, strawberries with apple juice.
+const outputCurrentPrice = document.getElementById('current-drink-price');
 
-const outputOrderDetails = document.getElementById("order-details"); // Display order details
-const outputItemPrice = document.getElementById("item-price"); // Display item price
+const outputOrderDetails = document.getElementById('order-details'); // Display order details
+const outputItemPrice = document.getElementById('item-price'); // Display item price
 
-const outputSubtotalPrice = document.getElementById("subtotal-price"); // Total Price value
-const outputFinalMessage = document.getElementById("final-message"); // Final message
+const outputSubtotalPrice = document.getElementById('subtotal-price'); // Total Price value
+const outputFinalMessage = document.getElementById('final-message'); // Final message
 
 // read input
 
 // output values
 
 // perform functions
+function PlaceOrder(Event){
+    if(theForm.checkValidity()){
+        Event.preventDefault();
+        outputFinalMessage.innerText = `Your order has been received, thumbs up from the restaurant!`;
+        theForm.reset();
+    }
+}
 
 // listen to events
+btnAddOrder.addEventListener('click', PlaceOrder);
+btnSaveFav.addEventListener('click', PlaceOrder);
+btnOrderFav.addEventListener('click', PlaceOrder);
+btnPlaceOrder.addEventListener('click', PlaceOrder); // button place order
+
+
